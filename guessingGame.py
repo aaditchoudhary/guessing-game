97de15fb32df333a7fbc003b861e8e39de690765
aaditchoudhary <67330928+aaditchoudhary@users.noreply.github.com>
2021-03-28 12:49:30 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,19 @@
+import random
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+random(a,b,c,d,e,f,g,h,i)
+number=random
+def guessNumber():
+    while chances<5:
+if guess == number:
+    print("Congratulations You Won!!!")
+    break
+if not chances<5:
+    print("You lose!! The number is", number)
